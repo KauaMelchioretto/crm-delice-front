f5ad32615f54dc56be6eeb84381578fc07d74e2d
unknown <kauamelchoretto@gmail.com>
2025-04-22 19:30:45 -0300

    continue user repository implementation

--- src/module/user/repository/UsersRepository.ts
+++ src/module/user/repository/UsersRepository.ts
@@ -17,31 +17,75 @@ class UsersRepository {
         try {
             const response = await http.post(
                 "...", {
-                    login: user.login,
-                    password: user.password,
-                    name: user.name,
-                    surname: user.surname,
-                    email: user.email,
-                    userType: user.userType,
-                    status: user.status,
-                    avatar: user.avatar,
-                    document: user.document,
-                    phone: user.phone,
-                    dateOfBirth: user.dateOfBirth,
-                    state: user.state,
-                    city: user.city,
-                    zipCode: user.zipCode,
-                    createdAt: user.createdAt,
-                    modifiedAt: user.modifiedAt
-                }
+                login: user.login,
+                password: user.password,
+                name: user.name,
+                surname: user.surname,
+                email: user.email,
+                userType: user.userType,
+                status: user.status,
+                avatar: user.avatar,
+                document: user.document,
+                phone: user.phone,
+                dateOfBirth: user.dateOfBirth,
+                state: user.state,
+                city: user.city,
+                zipCode: user.zipCode,
+                createdAt: user.createdAt,
+                modifiedAt: user.modifiedAt
+            }
             );
-            
+
             return { user: response.data?.module }
         } catch (e) {
             if (e instanceof AxiosError) {
-                return {error: e?.response?.data?.error?.message ?? this.MODULES_UNEXPECTED_ERROR}
+                return { error: e?.response?.data?.error?.message ?? this.MODULES_UNEXPECTED_ERROR }
             }
-            return {error: this.MODULES_UNEXPECTED_ERROR}
+            return { error: this.MODULES_UNEXPECTED_ERROR }
+        }
+    }
+
+    async getUsers(): Promise<UsersListResponse> {
+        try {
+            const response = await http.get(
+                "/user/getPagination"
+            );
+
+            return { users: response.data?.modules }
+        } catch (e) {
+            if (e instanceof AxiosError) {
+                return { error: e?.response?.data?.error?.message ?? this.MODULES_UNEXPECTED_ERROR }
+            }
+
+            return { error: this.MODULES_UNEXPECTED_ERROR }
+        }
+    }
+
+    async getUserByUUID(userUUID: string): Promise<UserResponse> {
+        try {
+            const response = await http.get(`/user/getByUUID/${userUUID}`);
+
+            return { user: response.data?.module }
+        } catch (e) {
+            if (e instanceof AxiosError) {
+                return { error: e?.response?.data?.error?.message ?? this.MODULES_UNEXPECTED_ERROR }
+            }
+
+            return { error: this.MODULES_UNEXPECTED_ERROR }
+        }
+    }
+
+    async getUserRoles(userUUID: string): Promise<UserWithRolesResponse> {
+        try {
+            const response = await http.get(`/roles/rolesPerUser/${userUUID}`);
+
+            return response.data as UserWithRolesResponse;
+        } catch (e) {
+            if (e instanceof AxiosError) {
+                return { error: e?.response?.data?.error?.message ?? this.MODULES_UNEXPECTED_ERROR }
+            }
+
+            return { error: this.MODULES_UNEXPECTED_ERROR };
         }
     }
 }
