942636610d1b16e831232d86802eb5b9cce7f124
unknown <kauamelchoretto@gmail.com>
2025-04-22 19:10:17 -0300

    start to implement users page

---
+++ src/module/user/repository/UsersRepository.ts
@@ -0,0 +1,47 @@
+import {
+    User,
+    UserResponse,
+    UsersListResponse,
+    UsersFormType,
+    UserDeleteResponse,
+    UserWithRolesResponse,
+} from "../entities/entities";
+import { Role, RoleResponse, RoleDeleteResponse } from "../../modules/enitites/entities";
+import { http } from "../../../core/config/api/http.ts";
+import { AxiosError } from "axios";
+
+class UsersRepository {
+    MODULES_UNEXPECTED_ERROR = "An unexpected erro has occurred";
+
+    async createUser(user: User): Promise<UserResponse> {
+        try {
+            const response = await http.post(
+                "...", {
+                    login: user.login,
+                    password: user.password,
+                    name: user.name,
+                    surname: user.surname,
+                    email: user.email,
+                    userType: user.userType,
+                    status: user.status,
+                    avatar: user.avatar,
+                    document: user.document,
+                    phone: user.phone,
+                    dateOfBirth: user.dateOfBirth,
+                    state: user.state,
+                    city: user.city,
+                    zipCode: user.zipCode,
+                    createdAt: user.createdAt,
+                    modifiedAt: user.modifiedAt
+                }
+            );
+            
+            return { user: response.data?.module }
+        } catch (e) {
+            if (e instanceof AxiosError) {
+                return {error: e?.response?.data?.error?.message ?? this.MODULES_UNEXPECTED_ERROR}
+            }
+            return {error: this.MODULES_UNEXPECTED_ERROR}
+        }
+    }
+}
