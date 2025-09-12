34682adc750d04a8729f579dec706660dd269c47
KauaMelchioretto <kauamelchoretto@gmail.com>
2025-09-12 18:54:10 -0300

    feature(): improve CrmTableHead component to support column fields without orderby action

--- src/utils/components/core/CrmTableHead.tsx
+++ src/utils/components/core/CrmTableHead.tsx
@@ -1,22 +1,23 @@
 import { Atom, useAtom, WritableAtom } from "jotai";
 import { CrmField, CrmOrderBy } from "../../entities/entities";
-import { ArrowDownwardRounded, ArrowUpwardRounded, FolderDelete } from "@mui/icons-material";
+import { ArrowDownwardRounded, ArrowUpwardRounded } from "@mui/icons-material";
+import { Tooltip, Typography } from "@mui/joy";
 
 interface CrmTableHeadProps {
   field: CrmField;
-  orderByAtom: Atom<CrmOrderBy | null>;
+  orderByAtom: Atom<CrmOrderBy | null> | null;
 }
 
-export const CrmTableHead = ({ field, orderByAtom }: CrmTableHeadProps) => {
+const CrmTableHeadField = ({ field, orderByAtom }: CrmTableHeadProps) => {
   const [orderBy, setOrderBy] = useAtom(
     orderByAtom as WritableAtom<CrmOrderBy | null, [CrmOrderBy], void>
   );
 
   const handleClickColumn = (field: string) => {
     if (orderBy?.ordenation === "asc") {
-      setOrderBy({ field: field, ordenation: "desc" });
+      setOrderBy({ field, ordenation: "desc" });
     } else {
-      setOrderBy({ field: field, ordenation: "asc" });
+      setOrderBy({ field, ordenation: "asc" });
     }
   };
 
@@ -25,13 +26,43 @@ export const CrmTableHead = ({ field, orderByAtom }: CrmTableHeadProps) => {
       style={{ cursor: "pointer" }}
       onClick={() => handleClickColumn(field.value)}
     >
-      <span title={field.value.toUpperCase()}>{field.label}</span>
-      {orderBy?.field === field.value &&
-        (orderBy.ordenation === "asc" ? (
-          <ArrowDownwardRounded fontSize="small" />
-        ) : (
-          <ArrowUpwardRounded fontSize="small" />
-        ))}
+      <Tooltip title={field.value.toUpperCase()} placement="top" size="sm">
+        <Typography
+          level="body-sm"
+          component="span"
+          sx={{ display: "inline-flex", alignItems: "center", gap: 0.5 }}
+        >
+          {field.label}
+          {orderBy?.field === field.value &&
+            (orderBy.ordenation === "asc" ? (
+              <ArrowDownwardRounded fontSize="small" />
+            ) : (
+              <ArrowUpwardRounded fontSize="small" />
+            ))}
+        </Typography>
+      </Tooltip>
     </th>
+  );
+};
+
+const CrmTableHeadAction = ({ field }: { field: CrmField }) => {
+  return (
+    <th>
+      <Typography
+        level="body-sm"
+        component="span"
+        sx={{ display: "inline-flex", alignItems: "center", gap: 0.5 }}
+      >
+        {field.label}
+      </Typography>
+    </th>
+  );
+};
+
+export const CrmTableHead = ({ field, orderByAtom }: CrmTableHeadProps) => {
+  return orderByAtom ? (
+    <CrmTableHeadField field={field} orderByAtom={orderByAtom} />
+  ) : (
+    <CrmTableHeadAction field={field} />
   );
 };
