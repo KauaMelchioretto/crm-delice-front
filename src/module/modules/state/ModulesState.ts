310c1e0c75e6f2cfa7c69b54a1702dfc00a8cfed
KauaMelchioretto <kauamelchoretto@gmail.com>
2025-09-16 18:06:46 -0300

    feature(): implements orderby, filter and pagination in module 'modules'

--- src/module/modules/state/ModulesState.ts
+++ src/module/modules/state/ModulesState.ts
@@ -2,20 +2,35 @@ import {atom} from "jotai";
 import {ModulesFormType} from "../enitites/entities.ts";
 import {modulesUseCase} from "../usecase/ModulesUseCase.ts";
 import {loadable} from "jotai/utils";
+import { CrmFilter, CrmOrderBy } from "../../../utils/entities/entities.ts";
 
 const ModulesFormTypeAtom = atom<ModulesFormType>(ModulesFormType.EMPTY);
-
+const PageAtom = atom(0)
+const FilterAtom = atom<CrmFilter | null>(null);
+const OrderByAtom = atom<CrmOrderBy | null>({field: "code", ordenation: "asc"})
 const ModuleFormUUIDAtom = atom("");
 
 const ModuleUpdateAtom = atom(false);
 
-const ModuleListAsyncAtom = atom(async (get) => {
+const ModuleListAtom = loadable(atom(async (get) => {
     get(ModuleUpdateAtom);
 
-    return modulesUseCase.getModules();
-})
+    const page = get(PageAtom)
+    const filter = get(FilterAtom);
+    const orderBy = get(OrderByAtom);
 
-const ModuleListAtom = loadable(ModuleListAsyncAtom);
+    return modulesUseCase.getModules(page, filter, orderBy);
+}));
+
+const ModulesListTotalCountAtom = loadable(atom(async (get) => {
+    const list = get(ModuleListAtom);
+
+    if (list.state === "hasData") {
+        return list.data.total ?? 0;
+    }
+
+    return 0;
+}))
 
 const AllRolesAsyncAtom = atom(async () => {
     return modulesUseCase.getAllRoles();
@@ -26,7 +41,11 @@ const AllRolesAtom = loadable(AllRolesAsyncAtom)
 export default {
     ModulesFormTypeAtom,
     ModuleFormUUIDAtom,
+    ModulesListTotalCountAtom,
     ModuleListAtom,
     ModuleUpdateAtom,
-    AllRolesAtom
+    AllRolesAtom,
+    PageAtom,
+    FilterAtom,
+    OrderByAtom
 }
